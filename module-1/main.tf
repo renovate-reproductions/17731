08ff61c00364851043a305f5f16d88b3e1578f1c
Josh Reichardt <josh.reichardt@gmail.com>
2022-09-10 03:46:30 +0000

    ci: test cross org base case

---
+++ module-1/main.tf
@@ -0,0 +1,3 @@
+module "test1" {
+  source = "git@github.com:terraform-aws-modules/terraform-aws-s3-bucket.git?ref=v2.15.0"
+}
---
+++ module-2/main.tf
@@ -0,0 +1,3 @@
+module "test" {
+  source = "git::git@github.com:gruntwork-io/terraform-aws-load-balancer.git//modules/alb?ref=v0.28.0"
+}
